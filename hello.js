b65f536e2a5d6ee6368cfae20069428445136cec
Tim <trmchale@gmail.com>
2015-01-02 17:54:41 -0500

    working with express get/post

--- hello.js
+++ hello.js
@@ -6,42 +6,36 @@ var app = express();
 
 app.use(morgan()); 
 
-app.use(function (request, response) {
-	if(request.url == "/") {
-		response.writeHead(200, { "Content-Type": "text/plain" });
-		response.end("Welcome to the home page")
-	}
-	// About page
-  else if (request.url == "/marketing") {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Welcome to the marketing page!");
-  }
+app.all("*", function(request, response, next){
+	response.writeHead(200, {"Content-Type" : "text/html"});
+	next();
+});
 
-  else if (request.url == "/uploads"){
-  	if(request.method == "POST"){
+app.get("/", function(request, response){
+	response.end("Welcome to the home page")
+});
+
+app.get("/uploads", function(request, response){
+          response.end('<html><head><title>UPLOADZ!</title><body><form action="/uploads" method="POST" enctype="multipart/form-data"><input type="file" name="the_file"><input type="submit" value="Upload">');
+});
+
+app.post("/uploads", function(request, response){
   		var upload = "";
   		request.on("data", function(chunk){
   			upload += chunk;
   			console.log("chunk: ", chunk)
-  		})
+  		});
 
   		request.on("end", function() {
   			console.log("Handling the upload: ", upload.length);
-  			response.writeHead(200, {'Content-Type': 'text/plain'});
   			response.end("Handling it!")
-  		})
-  	}
-  // 404'd!
-  else {
-          response.writeHead(200, {'Content-Type': 'text/html'});
-          response.end('<html><head><title>UPLOADZ!</title><body><form action="/uploads" method="POST" enctype="multipart/form-data"><input type="file" name="the_file"><input type="submit" value="Upload">');
-        }
-      } else {
-        response.writeHead(404, {'Content-Type': 'text/plain'});
-        response.end('Page not found.\n');
-      }
+  		});
+  	});
 
+app.get("*", function(request, response) {
+	response.end('404 -- Page not found.\n');
 });
+
 
 http.createServer(app).listen(8000);
 
